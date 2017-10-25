2672f36ac4595aaec7d90de16fb1be509a4d3feb
macacos2 <lucascoliado@hotmail.com>
2017-10-25 20:01:18 -0200

    Adicionar biblioteca in-dev para identificar tabelas

---
+++ dev/parsers.py
@@ -0,0 +1,26 @@
+import utils.py
+import json
+
+"""	table = html.find('table')
+	tableDic = {}
+
+	categoria = table.find('caption').get_text()
+	tableDic.update({"categoria":categoria})
+	for row in table.find_all('tr'):
+		linha = row.find_all('td')
+		if len(linha) > 3:
+			if subgrupo is not None:
+				subgrupo = {"subgrupo":linha[0].get_text()}
+				count=0;
+			else
+				tableDic.update({subgrupo["subgrupo"]:subgrupo})
+				subgrupo = {"subgrupo":linha[0].get_text()}
+				count=0;
+		
+		
+			tituloClassificacao = "classificacao"+count
+			count+=1
+			subgrupo.update({tituloClassificacao:
+
+	print(json.dumps(tableDic, indent = 4))
+"""
--- dev/run.py
+++ dev/run.py
@@ -1,18 +1,32 @@
 from utils import getHtmlSoup
 import os
+import sys
 
 print("DEV mode on")
-#
+# Constantes #
 nomeDiretorioEscrita = "output"
 nomeArquivo = "paginaTarifa"
 paginaTarifas = "http://www.celesc.com.br/portal/index.php/duvidas-mais-frequentes/1140-tarifa"
-
+separator = "****"
 diretorioEscrita = os.path.join(os.path.dirname(__file__), nomeDiretorioEscrita)
 diretorioArquivo = os.path.join(diretorioEscrita, nomeArquivo)
-#
+##############
+#arquivo = open(diretorioArquivo, "w")
 
-arquivo = open(diretorioArquivo, "w")
-arquivo.write(getHtmlSoup(paginaTarifas).prettify())
-arquivo.close()
+html = getHtmlSoup(paginaTarifas)
+tabelas = []
+
+for table in html.find_all('table'):
+	print("\n" + separator)
+	titulo = table.find_all('caption')[0].get_text()
+	print("titulo: " + titulo)
+	for row in table.find_all('tr'):
+		for cell in row.find_all('td'):
+			if cell.get_text() != "":
+				print(cell.get_text(), end='~')
+		print("\n")
+
+#arquivo.write(getHtmlSoup(paginaTarifas).prettify())
+#arquivo.close()
 
 print("printed")
